80402529171e45adfac5bb122dd562e97172b706
mooz <stillpedant@gmail.com>
2011-12-19 14:24:14 +0900

    Attribute name is missed in selection operator.

--- src/currentia/core/operator/selection.h
+++ src/currentia/core/operator/selection.h
@@ -14,34 +14,36 @@ namespace currentia {
     class SelectionOperator: public Operator {
         // TODO: support condition from multiple comparision (AND, OR, BETWEEN, ...)
         enum ComparatorType comparator_;
+        std::string target_attribute_name_;
         Object comparator_value_;
 
     public:
         // created from visitor pattern?
         // TODO: think about the way of construction from declarative queries
-        SelectionOperator(Stream::ptr_t input_stream,
+        SelectionOperator(Stream::ptr_t input_stream_ptr,
                           enum ComparatorType comparator,
+                          std::string target_attribute_name,
                           Object comparator_value):
-            Operator(input_stream), // constructor of the super class
+            Operator(input_stream_ptr), // constructor of the super class
             comparator_(comparator),
+            target_attribute_name_(target_attribute_name),
             comparator_value_(comparator_value) {
         }
 
         Tuple::ptr_t next() {
-            std::string target_column_name = "";
-            int target_column_index = -1;
+            int target_attribute_index = -1;
 
-            while (Tuple::ptr_t target_tuple_ptr = input_stream_->dequeue()) {
+            while (Tuple::ptr_t target_tuple_ptr = input_stream_ptr_->dequeue()) {
                 // we assume branch prediction of the processor pass this comparation
-                if (target_column_index == -1) {
+                if (target_attribute_index == -1) {
                     // for fast access to the attributes
-                    target_column_index = target_tuple_ptr->
-                                          schema_ptr_->
-                                          attributes_index_[target_column_name];
+                    target_attribute_index = target_tuple_ptr->
+                                             schema_ptr_->
+                                             attributes_index_[target_attribute_name_];
                 }
 
                 // always compared by [target_value <comparator_> comparator_value_]
-                Object target_value = target_tuple_ptr->data_[target_column_index];
+                Object target_value = target_tuple_ptr->data_[target_attribute_index];
 
                 if (target_value.compare(comparator_value_, comparator_)) {
                     // return tuple which fulfills condition
