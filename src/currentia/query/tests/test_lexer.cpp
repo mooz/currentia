79c676344042687a3f26ae6045ae182f50b08282
mooz <stillpedant@gmail.com>
2012-01-30 18:45:46 +0900

    Fixed test

--- src/currentia/query/tests/test_lexer.cpp
+++ src/currentia/query/tests/test_lexer.cpp
@@ -18,8 +18,27 @@ TEST (testLexer, whitespace) {
     EXPECT_EQ(Lexer::EOS, lexer.next_token());
 }
 
+TEST (testLexer, comment) {
+   std::istringstream is("SELECT # SELECT FROM WHERE"); 
+    currentia::Lexer lexer(&is);
+
+    EXPECT_EQ(Lexer::SELECT, lexer.next_token());
+    EXPECT_EQ(Lexer::EOS, lexer.next_token());
+}
+
+
 TEST (testLexer, keywords) {
     std::istringstream is("SELECT FROM WHERE");
+    currentia::Lexer lexer(&is);
+
+    EXPECT_EQ(Lexer::SELECT, lexer.next_token());
+    EXPECT_EQ(Lexer::FROM, lexer.next_token());
+    EXPECT_EQ(Lexer::WHERE, lexer.next_token());
+    EXPECT_EQ(Lexer::EOS, lexer.next_token());
+}
+
+TEST (testLexer, keywordsIgnoreCase) {
+    std::istringstream is("select frOM wHere");
     currentia::Lexer lexer(&is);
 
     EXPECT_EQ(Lexer::SELECT, lexer.next_token());
@@ -33,13 +52,13 @@ TEST (testLexer, name) {
     currentia::Lexer lexer(&is);
 
     EXPECT_EQ(Lexer::NAME, lexer.next_token());
-    EXPECT_EQ("foo", lexer.get_latest_name());
+    EXPECT_EQ("FOO", lexer.get_latest_name());
 
     EXPECT_EQ(Lexer::NAME, lexer.next_token());
-    EXPECT_EQ("bar", lexer.get_latest_name());
+    EXPECT_EQ("BAR", lexer.get_latest_name());
 
     EXPECT_EQ(Lexer::NAME, lexer.next_token());
-    EXPECT_EQ("baz", lexer.get_latest_name());
+    EXPECT_EQ("BAZ", lexer.get_latest_name());
 
     EXPECT_EQ(Lexer::EOS, lexer.next_token());
 }
