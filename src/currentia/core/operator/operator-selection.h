34ec6cc76cb1ccc3cb70e29b61b1f1031c368c2b
mooz <stillpedant@gmail.com>
2012-02-16 19:06:27 +0900

    Now Selection operator can calculate its selectivity

--- src/currentia/core/operator/operator-selection.h
+++ src/currentia/core/operator/operator-selection.h
@@ -16,30 +16,43 @@ namespace currentia {
         Operator::ptr_t parent_operator_ptr_;
         Schema::ptr_t output_schema_ptr_;
 
+        int input_tuple_count_;
+        int selected_tuple_count_;
+
     public:
         OperatorSelection(Operator::ptr_t parent_operator_ptr,
                           Condition::ptr_t condition_ptr):
-            condition_ptr_(condition_ptr) {
+            condition_ptr_(condition_ptr),
+            input_tuple_count_(0),
+            selected_tuple_count_(0) {
             parent_operator_ptr_ = parent_operator_ptr;
             output_schema_ptr_ = parent_operator_ptr_->get_output_schema_ptr();
         }
 
-        inline
         Schema::ptr_t get_output_schema_ptr() {
             return output_schema_ptr_;
         }
 
-        inline
         Tuple::ptr_t next_implementation() {
             while (Tuple::ptr_t target_tuple_ptr = parent_operator_ptr_->next()) {
                 if (target_tuple_ptr->is_system_message())
                     return target_tuple_ptr;
 
-                if (condition_ptr_->check(target_tuple_ptr))
+                input_tuple_count_++;
+
+                if (condition_ptr_->check(target_tuple_ptr)) {
+                    selected_tuple_count_++;
                     return target_tuple_ptr;
+                }
                 // otherwise, tuple will be *deleted* by the shared_ptr
             }
             return Tuple::ptr_t(); // NULL
+        }
+
+        double get_selectivity() {
+            if (input_tuple_count_ == 0)
+                return 0.0;
+            return input_tuple_count_ / selected_tuple_count_;
         }
     };
 }
