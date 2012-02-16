26646baf0eb97309715f4155b4c8f916b75f00f1
mooz <stillpedant@gmail.com>
2012-02-16 19:06:03 +0900

    Implement hook system to Operator class

--- src/currentia/core/operator/operator.h
+++ src/currentia/core/operator/operator.h
@@ -15,15 +15,33 @@ namespace currentia {
     class Operator: private NonCopyable<Operator>,
                     public Pointable<Operator> {
     public:
+        typedef void (*process_hook_t)(const Tuple::ptr_t&);
+
         // get next tuple from input stream and process
         Tuple::ptr_t next() {
             Tuple::ptr_t result = next_implementation();
+
+            if (!after_process_hook.empty()) {
+                for (std::list<process_hook_t>::iterator process_iterator = after_process_hook.begin();
+                     process_iterator != after_process_hook.end();
+                     ++process_iterator) {
+                    (*process_iterator)(result);
+                }
+            }
+
             return result;
         }
 
         virtual Tuple::ptr_t next_implementation() = 0; // blocking operator
 
         virtual Schema::ptr_t get_output_schema_ptr() = 0;
+
+        void add_after_process(process_hook_t hook) {
+            after_process_hook.push_back(hook);
+        }
+
+    private:
+        std::list<process_hook_t> after_process_hook;
     };
 }
 
