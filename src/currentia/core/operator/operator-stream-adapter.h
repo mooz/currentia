84e8b4107e2dcf48f512a7c61d9f51b16d8310e2
mooz <stillpedant@gmail.com>
2012-06-24 20:54:13 +0900

    Push style OperatorStreamAdapter

--- src/currentia/core/operator/operator-stream-adapter.h
+++ src/currentia/core/operator/operator-stream-adapter.h
@@ -3,31 +3,24 @@
 #ifndef CURRENTIA_OPERATOR_STREAM_ADAPTER_H_
 #define CURRENTIA_OPERATOR_STREAM_ADAPTER_H_
 
-#include "currentia/core/tuple.h"
-#include "currentia/core/stream.h"
 #include "currentia/core/object.h"
-
 #include "currentia/core/operator/operator.h"
+#include "currentia/core/stream.h"
+#include "currentia/core/tuple.h"
 
 namespace currentia {
     class OperatorStreamAdapter: public Operator {
         Stream::ptr_t input_stream_ptr_;
-        Schema::ptr_t output_schema_ptr_;
 
     public:
         OperatorStreamAdapter(Stream::ptr_t input_stream_ptr):
             input_stream_ptr_(input_stream_ptr) {
-            output_schema_ptr_ = input_stream_ptr->get_schema_ptr();
+            set_output_stream(Stream::from_schema(input_stream_ptr->get_schema_ptr()));
         }
 
-        inline
-        Schema::ptr_t get_output_schema_ptr() {
-            return output_schema_ptr_;
-        }
-
-        inline
         Tuple::ptr_t next_implementation() {
-            return input_stream_ptr_->dequeue();
+            output_tuple(input_stream_ptr_->dequeue());
+            return Tuple::ptr_t();
         }
     };
 }
