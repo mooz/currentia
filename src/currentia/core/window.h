b7b6f137f9939541fe47246666f8932cdad2732c
mooz <stillpedant@gmail.com>
2012-06-28 00:35:13 +0900

    Add Window::toString()

--- src/currentia/core/window.h
+++ src/currentia/core/window.h
@@ -3,8 +3,11 @@
 #ifndef CURRENTIA_WINDOW_H_
 #define CURRENTIA_WINDOW_H_
 
+#include <string>
+#include <sstream>
+
 namespace currentia {
-    class Window {
+    struct Window {
     public:
         enum Type {
             TUPLE_BASE,
@@ -21,6 +24,25 @@ namespace currentia {
             type(type) {
             if (stride < 1)
                 stride = 1;
+        }
+
+        Window() {}
+
+        std::string toString() const {
+            std::stringstream ss;
+
+            ss << "[ width " << width << " | stride " << stride << "] ";
+
+            switch (type) {
+            case TUPLE_BASE:
+                ss << "(TUPLE)";
+                break;
+            case TIME_BASE:
+                ss << "(TIME)";
+                break;
+            }
+
+            return ss.str();
         }
     };
 }
