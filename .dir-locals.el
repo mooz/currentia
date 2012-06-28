b3bf397af2890f4024d57d71fa6f526cf8291772
mooz <stillpedant@gmail.com>
2012-06-29 00:28:23 +0900

    Run flymake in C++0x mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,1 @@
-((c++-mode . ((flymake-additional-compilation-flags . ("-I/home/masa/src/currentia/src/")))))
+((c++-mode . ((flymake-additional-compilation-flags . ("-I/home/masa/src/currentia/src/" "-std=gnu++0x")))))
