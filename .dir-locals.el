124203f143877f3f1a35ba44249341de82c744aa
mooz <stillpedant@gmail.com>
2012-12-03 01:26:21 +0900

    Merge .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,2 @@
-((c++-mode . ((flymake-additional-compilation-flags . ("-I/home/masa/src/currentia/src/" "-std=gnu++0x" "-DCURRENTIA_ENABLE_TRANSACTION" "-DCURRENTIA_ENABLE_TIME_BASED_WINDOW")))))
+((c++-mode . ((flymake-additional-compilation-flags . ("-I/home/masa/src/currentia/src/" "-std=gnu++0x" "-DCURRENTIA_ENABLE_TRANSACTION" "-DCURRENTIA_ENABLE_TIME_BASED_WINDOW"))
+              (ac-clang-cflags . ("-I/home/masa/src/currentia/src/" "-std=gnu++0x" "-DCURRENTIA_ENABLE_TRANSACTION" "-DCURRENTIA_ENABLE_TIME_BASED_WINDOW")))))
