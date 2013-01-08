5662de6394c914c359c67738c4eadd6b8c57826f
mooz <stillpedant@gmail.com>
2013-01-09 01:02:12 +0900

    Add batch_count_ to AbstractScheduler

--- src/currentia/core/scheduler/abstract-scheduler.h
+++ src/currentia/core/scheduler/abstract-scheduler.h
@@ -9,6 +9,9 @@
 
 namespace currentia {
     class AbstractScheduler {
+    public:
+        int batch_count_;       // (maximum) number of tuples to be evaluated at a chance
+
     protected:
         const Operator::ptr_t root_operator_;
         const std::vector<Operator*> operators_;
@@ -17,11 +20,20 @@ namespace currentia {
     public:
         AbstractScheduler(const Operator::ptr_t& root_operator,
                           const SchedulingPolicyFactory::ptr_t& scheduling_policy_factory):
+            batch_count_(1),
             root_operator_(root_operator),
             operators_(OperatorVisitorSerializer::serialize_tree(root_operator.get())),
             // To make scheduler free its policy in destruction phase,
             // wrap policy with shared_ptr
             scheduling_policy_(SchedulingPolicy::ptr_t(scheduling_policy_factory->create_policy_in_heap(root_operator))) {
+        }
+
+        int batch_count() const {
+            return batch_count_;
+        }
+
+        void set_batch_count(int batch_count) {
+            batch_count_ = batch_count;
         }
 
         virtual ~AbstractScheduler() = 0;
