afa074866628b2dd56d3d0b6142413ca3a47e984
mooz <stillpedant@gmail.com>
2013-01-10 00:01:07 +0900

    #if 0 -> #if CURRENTIA_DEBUG

--- src/currentia/core/cc/redo-area.h
+++ src/currentia/core/cc/redo-area.h
@@ -65,14 +65,14 @@ namespace currentia {
 
         void make_operator_output_stream_backedup(Operator* op) {
             redo_streams_.push_back(op->get_output_stream());
-#if 0
+#if CURRENTIA_DEBUG
             std::clog << "Make operator " << op->get_name() << "'s input backup" << std::endl;
 #endif
             op->get_output_stream()->set_backup_state(true);
         }
 
         void push_operator_into_redo_area(Operator* op) {
-#if 0
+#if CURRENTIA_DEBUG
             std::clog << "Push operator " << op->get_name() << " into redo area" << std::endl;
 #endif
             operators_in_redo_area_.push_back(op);
