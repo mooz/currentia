d5ecfdd6dfb0874f261383535529cfc861110e23
mooz <stillpedant@gmail.com>
2013-01-10 14:54:30 +0900

    Sophisticate dot output

--- src/currentia/core/operator/operator-visitor-visualize-dot.h
+++ src/currentia/core/operator/operator-visitor-visualize-dot.h
@@ -19,9 +19,6 @@ namespace currentia {
     // Serialize a operator tree by topological sort algorithm
     class OperatorVisualizeDot : public OperatorAbstractVisitor,
                                  public Pointable<OperatorVisualizeDot> {
-        std::list<Operator*> operator_stack_;
-        std::list<Operator*> operator_list_;
-        std::map<Operator*, Operator*> child_map_;
         std::map<std::string, int> duplication_count_map_;
         std::map<Operator*, int> operator_id_map_;
         std::ostream& dot_output_;
@@ -40,15 +37,12 @@ namespace currentia {
             OperatorVisualizeDot dotter(dot_output);
             dot_output << "digraph " << query_name << " {" << std::endl;
             dotter.dispatch(root_operator_ptr.get());
-            dotter.prettify_graph();
             dot_output << "}" << std::endl;
         }
 
         // Push / Pop
 
-        void push_operator(Operator* op) {
-            operator_stack_.push_back(op);
-
+        void record_operator(Operator* op) {
             std::string op_name = op->get_name();
             if (duplication_count_map_.find(op_name) == duplication_count_map_.end()) {
                 duplication_count_map_[op_name] = 0;
@@ -56,13 +50,23 @@ namespace currentia {
                 duplication_count_map_[op_name] = duplication_count_map_[op_name] + 1;
             }
             operator_id_map_[op] = duplication_count_map_[op_name];
+
+            if (op->is_commit_operator()) {
+                dot_output_ << "  " << get_node_name_from_operator(op) << " [style=\"filled\",color=\"yellow\"];" << std::endl;
+            }
         }
 
-        void pop_operator() {
-            Operator* op = operator_stack_.back();
-            operator_stack_.pop_back();
-            // Also, collect all operators
-            operator_list_.push_back(op);
+        void connect_edge(Operator* from, Operator* to) {
+            dot_output_ << "  "
+                        << get_node_name_from_operator(from)
+                        << " -> "
+                        << get_node_name_from_operator(to);
+
+            if (from->get_output_stream()->is_backed_up()) {
+                dot_output_ << " [label=\"Has Backup\"]" << std::endl;
+            }
+
+            dot_output_ << ";" << std::endl;
         }
 
         // Print
@@ -77,62 +81,29 @@ namespace currentia {
             return ss.str();
         }
 
-        void print_operator_stack() {
-            auto iter = operator_stack_.rbegin();
-            auto iter_end = operator_stack_.rend();
-            dot_output_ << "  ";
-            for (; iter != iter_end;) {
-                Operator* op = (*iter);
-                dot_output_ << get_node_name_from_operator(op);
-                if (++iter == iter_end)
-                    break;
-                dot_output_ << " -> ";
-            }
-            dot_output_ << ";" << std::endl;
-        }
-
-        void prettify_graph() {
-            auto iter = operator_list_.begin();
-            auto iter_end = operator_list_.end();
-            for (; iter != iter_end; ++iter) {
-                Operator* op = (*iter);
-                if (op->is_commit_operator()) {
-                    dot_output_ << "  " << get_node_name_from_operator(op) << " [style=\"filled\",color=\"yellow\"];" << std::endl;
-                }
-                if (op->get_output_stream()->is_backed_up() &&
-                    child_map_.find(op) != child_map_.end()) {
-                    dot_output_ << "  " << get_node_name_from_operator(op) << " -> "
-                                << get_node_name_from_operator(child_map_[op]) << " [label=\"Backup\"];" << std::endl;
-                }
-            }
-        }
-
         // Traversing
 
         void visit(SingleInputOperator* op) {
-            push_operator(op);
+            record_operator(op);
             Operator* parent = op->get_parent_operator().get();
             dispatch(parent);
-            child_map_[parent] = op;
-            pop_operator();
+
+            connect_edge(parent, op);
         }
 
         void visit(DoubleInputOperator* op) {
-            push_operator(op);
+            record_operator(op);
             Operator* left_parent = op->get_parent_left_operator().get();
             Operator* right_parent = op->get_parent_right_operator().get();
             dispatch(left_parent);
             dispatch(right_parent);
-            child_map_[left_parent] = op;
-            child_map_[right_parent] = op;
-            pop_operator();
+
+            connect_edge(left_parent, op);
+            connect_edge(right_parent, op);
         }
 
         void visit(OperatorStreamAdapter* op) {
-            push_operator(op);
-            // Leaf node. Output current stack.
-            print_operator_stack();
-            pop_operator();
+            record_operator(op);
         }
 
         CURRENTIA_DEFINE_OPERATOR_DISPATCHER();
