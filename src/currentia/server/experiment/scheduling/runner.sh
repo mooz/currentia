c7312c303ee345ba0b97c858af5d63d37ab21b2b
mooz <stillpedant@gmail.com>
2013-01-12 22:07:27 +0900

    Use 2PL in experiments

--- src/currentia/server/experiment/scheduling/runner.sh
+++ src/currentia/server/experiment/scheduling/runner.sh
@@ -5,4 +5,4 @@ EXPERIMENT_BIN=$(git rev-parse --show-cdup)build/src/currentia/server/experiment
 cat scheduling_query_template.cpl | \
     sed s"/!range!/${WIDTH}/" | \
     sed s"/!slide!/${SLIDE}/" | \
-    ${EXPERIMENT_BIN} --no-color --method=optimistic --max-events-n-consume=${BATCH_SIZE} --update-interval=100000 $*
+    ${EXPERIMENT_BIN} --no-color --method=2pl --max-events-n-consume=${BATCH_SIZE} --update-interval=100000 $*
