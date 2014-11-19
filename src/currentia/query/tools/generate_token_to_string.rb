f56e42c5005a94c05a54fa2a9fb5a9752ea1e113
mooz <stillpedant@gmail.com>
2014-11-19 17:16:13 +0900

    Use IO#each_line instead of IO#lines, which is now deprecated

--- src/currentia/query/tools/generate_token_to_string.rb
+++ src/currentia/query/tools/generate_token_to_string.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-STDIN.lines.each { |line|
+STDIN.each_line { |line|
   matched = line.match(/#define ([a-zA-Z0-9_]+)/)
   puts "case #{matched[1]}: return \"#{matched[1]}\";" if matched
 }
